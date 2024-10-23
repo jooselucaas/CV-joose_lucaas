f03df5aca33864e1bf568dbaf4e213193be4405b
josé lucas <99686443+jooselucaas@users.noreply.github.com>
2024-10-23 17:42:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// Função para alternar o menu
 function toggleMenu() {
     const navbar = document.getElementById('navbar');
     const menuIcon = document.getElementById('menuIcon');
     const welcomeMessage = document.querySelector('.welcome-message');
     const hrElement = document.querySelector('.header-line');
+    const body = document.body; // Adiciona a referência ao body
 
     navbar.classList.toggle('open');
+
+    // Alterna a classe 'no-scroll' no body
+    body.classList.toggle('no-scroll', navbar.classList.contains('open'));
 
     if (navbar.classList.contains('open')) {
         menuIcon.innerHTML = '✖';
@@ -17,6 +22,7 @@ function toggleMenu() {
     }
 }
 
+// Função para alternar o tema
 function toggleTheme() {
     const body = document.body;
     const currentTheme = localStorage.getItem('theme');
@@ -30,6 +36,7 @@ function toggleTheme() {
     }
 }
 
+// Executa ao carregar a página
 window.onload = () => {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
@@ -40,6 +47,7 @@ window.onload = () => {
     const welcomeMessage = document.querySelector('.welcome-message');
     const hrElement = document.querySelector('.header-line');
 
+    // Configuração inicial da navbar
     if (navbar.classList.contains('open')) {
         welcomeMessage.style.display = 'block';
         hrElement.classList.add('hidden');
@@ -49,5 +57,27 @@ window.onload = () => {
     }
 };
 
-// Adiciona o evento de clique ao botão de troca de tema
-document.getElementById('themeToggleButton').addEventListener('click', toggleTheme);
+
+const modal = document.getElementById('project-modal');
+const closeModal = document.querySelector('.close');
+
+document.querySelectorAll('.ver-mais').forEach(button => {
+    button.addEventListener('click', () => {
+        modal.style.display = 'block';
+        const project = button.dataset.project;
+        if (project === 'movieGuide') {
+            modal.querySelector('h1').textContent = 'The Movie Guide';
+            modal.querySelector('h3').textContent = '17 de Março de 2022';
+        }
+    });
+});
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
